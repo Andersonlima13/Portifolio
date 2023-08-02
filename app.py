39f53ae696846d1ca2e7912324a37ea433b89c4b
unknown <anderson.lima18pb@gmail.com>
2023-08-02 15:51:40 -0300

    dot-env commit

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, redirect , request , flash
 from flask_mail import Mail, Message
-from config import email,senha
+from dotenv import load_dotenv
+import os 
+load_dotenv()
+
 
 
 app = Flask(__name__)
@@ -11,8 +14,8 @@ mail_settings = {
     "MAIL_PORT": 465,
     "MAIL_USE_TLS": False,
     "MAIL_USE_SSL": True,
-    "MAIL_USERNAME": email,
-    "MAIL_PASSWORD" : senha
+    "MAIL_USERNAME": os.getenv("EMAIL"),
+    "MAIL_PASSWORD" : os.getenv("SENHA")
     
 }
 
@@ -21,8 +24,8 @@ mail = Mail(app)
 
 class Contato:
     def __init__(self,nome,email,mensagem):
-        self.nome = nome,
-        self.email = email,
+        self.nome = nome
+        self.email = email
         self.mensagem = mensagem
 
 
--- config.py
+++
@@ -1,2 +0,0 @@
-email = "anderson.lima18pb@gmail.com"
-senha = "anderson1709"
